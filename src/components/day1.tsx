9a06c26b72e4a683ea5957032ed5b957a1598bf3
Silas Windhorst <silaswind1123@gmail.com>
2023-12-02 00:49:11 +0100

    day1 code

--- src/components/day1.tsx
+++ src/components/day1.tsx
@@ -3,7 +3,20 @@ import { TextField, Button } from "@mui/material";
 
 function DayOne() {
   const [inputString, setInputString] = useState("");
-  const [input, setInput] = useState([""]);
+  const [output, setOutput] = useState(0);
+
+  const numbers = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -12,8 +25,51 @@ function DayOne() {
   };
 
   const convertInput = () => {
-    console.log(inputString.split(/\r?\n/));
-    setInput(inputString.split(/\r?\n/));
+    const inputArray = inputString.split(/\r?\n/);
+    let output = 0;
+    inputArray.forEach((el) => {
+      const reversedString = el.split("").reverse().join("");
+      let firstNumber = 100;
+      let lastNumber = -100;
+      let firstDigitIndex = el.search(/\d/);
+      let firstDigit = el[firstDigitIndex];
+      let lastDigitIndex =
+        reversedString.search(/\d/) !== -1
+          ? el.length - reversedString.search(/\d/) - 1
+          : -1;
+      let lastDigit = el[lastDigitIndex];
+      let lowestWordFoundIndex = 100;
+      let lowestWordFound = "";
+      let highestWordFoundIndex = -100;
+      let highestWordFound = "";
+      numbers.forEach((number) => {
+        const index = el.indexOf(number);
+        const lastIndex = el.lastIndexOf(number);
+        if (index !== -1) {
+          if (index < lowestWordFoundIndex) {
+            lowestWordFoundIndex = index;
+            lowestWordFound = number;
+          }
+          if (lastIndex > highestWordFoundIndex) {
+            highestWordFoundIndex = lastIndex;
+            highestWordFound = number;
+          }
+        }
+      });
+      if (firstDigitIndex !== -1 && firstDigitIndex < lowestWordFoundIndex) {
+        firstNumber = parseInt(firstDigit);
+      } else {
+        firstNumber = numbers.indexOf(lowestWordFound);
+      }
+      if (lastDigitIndex > highestWordFoundIndex) {
+        lastNumber = parseInt(lastDigit);
+      } else {
+        lastNumber = numbers.indexOf(highestWordFound);
+      }
+      output =
+        output + parseInt(firstNumber.toString() + lastNumber.toString());
+    });
+    setOutput(output);
   };
   return (
     <div>
@@ -23,10 +79,12 @@ function DayOne() {
         rows={4}
         value={inputString}
         onChange={(e) => handleChange(e)}
+        sx={{ width: "90vw" }}
       />
       <Button onClick={convertInput} variant="contained">
         Convert
       </Button>
+      <h4 style={{ marginTop: 5 }}>Sum of all numbers: {output}</h4>
     </div>
   );
 }
