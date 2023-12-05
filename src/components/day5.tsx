ea7180a819b77135e3448eeffa979378a0501029
Silas Windhorst <silas.windhorst@tuhh.de>
2023-12-05 21:01:18 +0100

    day 5 part 2 insanity

--- src/components/day5.tsx
+++ src/components/day5.tsx
@@ -33,46 +33,82 @@ function DayFive() {
         return parseInt(seed);
       });
     if (part2) {
-      let newSeeds = [...seeds];
-      seeds = [];
-      newSeeds.forEach((seed, index) => {
-        if (index % 2 === 0) {
-          for (let i = 0; i < newSeeds[index + 1]; i++) {
-            seeds.push(newSeeds[index] + i);
-          }
+      const seedGroups: number[][] = [];
+      for (let i = 0; i < seeds.length; i += 2) {
+        if (i + 1 < seeds.length) {
+          seedGroups.push([seeds[i], seeds[i + 1]]);
+        } else {
+          seedGroups.push([seeds[i]]);
+        }
+      }
+      // let lowestConversion = extractArray(6, match)[0];
+      // extractArray(6, match).forEach((conversion) => {
+      //   if (
+      //     conversion[0] < lowestConversion[0]
+      //   ) {
+      //     lowestConversion = conversion;
+      //   }
+      // });
+      // console.log(lowestConversion);
+      const totalPossibleSeeds: number[][] = [];
+      extractArray(6, match).forEach((lastConversion) => {
+        let possibleConversions: number[][] = [];
+        let lastPossibleConversions: number[][] = [lastConversion];
+        for (let i = 5; i > -1; i--) {
+          extractArray(i, match).forEach((conversion) => {
+            lastPossibleConversions.forEach((possibleConversion) => {
+              if (
+                conversion[0] >= possibleConversion[1] &&
+                conversion[0] <= possibleConversion[1] + possibleConversion[2]
+              ) {
+                possibleConversions.push(conversion);
+              }
+            });
+          });
+          lastPossibleConversions = [...possibleConversions];
+        }
+        const possibleSeeds: number[][] = [];
+        console.log(possibleConversions);
+        // seedGroups.forEach((group) => {
+        //   if (
+        //     group[0] >= lowestConversion[1] &&
+        //     group[0] + group[1] <= lowestConversion[1] + lowestConversion[2]
+        //   ) {
+        //     possibleSeeds.push(group);
+        //   }
+        // });
+        // if (possibleSeeds.length) {
+        //   possibleSeeds.map((seed) => totalPossibleSeeds.push(seed));
+        // }
+      });
+      console.log(totalPossibleSeeds);
+    } else {
+      let locations: number[] = [];
+      seeds.forEach((seed) => {
+        let newSeed = seed;
+        for (let i = 0; i < 7; i++) {
+          let changed = false;
+          extractArray(i, match).forEach((conversion) => {
+            if (
+              newSeed <= conversion[1] + conversion[2] - 1 &&
+              newSeed >= conversion[1] &&
+              !changed
+            ) {
+              newSeed = conversion[0] + newSeed - conversion[1];
+              changed = true;
+            }
+          });
+        }
+        locations.push(newSeed);
+      });
+      let smallestLocation = locations[0];
+      locations.forEach((location) => {
+        if (location < smallestLocation) {
+          smallestLocation = location;
         }
       });
+      setOutput(smallestLocation);
     }
-    let locations: number[] = [];
-    seeds.forEach((seed) => {
-      let newSeed = seed;
-      for (let i = 0; i < 7; i++) {
-        let changed = false;
-        extractArray(i, match).forEach((conversion, index) => {
-          if (
-            newSeed <= conversion[1] + conversion[2] - 1 &&
-            newSeed >= conversion[1] &&
-            !changed
-          ) {
-            // console.log(
-            //   `changed ${newSeed} to ${
-            //     conversion[0] + newSeed - conversion[1]
-            //   } in step ${i} conversion ${index}`
-            // );
-            newSeed = conversion[0] + newSeed - conversion[1];
-            changed = true;
-          }
-        });
-      }
-      locations.push(newSeed);
-    });
-    let smallestLocation = locations[0];
-    locations.forEach((location) => {
-      if (location < smallestLocation) {
-        smallestLocation = location;
-      }
-    });
-    setOutput(smallestLocation);
   };
   return (
     <Box
