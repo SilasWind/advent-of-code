429e0e377d641e32550196a318754898c4ee35df
Silas Windhorst <silaswind1123@gmail.com>
2023-12-06 14:46:55 +0100

    day 6 part 1 and 2

--- src/components/day6.tsx
+++ src/components/day6.tsx
@@ -21,18 +21,27 @@ function DaySix() {
   const doTheThing = () => {
     const match =
       inputString.match(/Time:\s+([\d\s]+)Distance:\s+([\d\s]+)/) ?? [];
-    const times = match[1]
-      ?.split(" ")
-      .filter((time) => time)
-      .map((time) => parseInt(time.trim()));
-    const distances = match[2]
-      ?.split(" ")
-      .filter((distance) => distance)
-      .map((distance) => parseInt(distance.trim()));
-    let foundDistances: number[] = new Array(distances.length);
+    const times = part2
+      ? [parseInt(match[1]?.replaceAll(" ", ""))]
+      : match[1]
+          ?.split(" ")
+          .filter((time) => time)
+          .map((time) => parseInt(time.trim()));
+    const distances = part2
+      ? [parseInt(match[2]?.replaceAll(" ", ""))]
+      : match[2]
+          ?.split(" ")
+          .filter((distance) => distance)
+          .map((distance) => parseInt(distance.trim()));
+    let foundDistances: number[] = new Array(distances.length).fill(0);
     times.forEach((time, index) => {
-      for (let i = 0; i <= time; i++) {}
+      for (let i = 0; i <= time; i++) {
+        if (i * (time - i) > distances[index]) {
+          foundDistances[index]++;
+        }
+      }
     });
+    setOutput(foundDistances.reduce((a, b) => a * b, 1));
   };
   return (
     <Box
@@ -67,7 +76,7 @@ function DaySix() {
       ) : (
         <Box>
           <h4 style={{ marginTop: 5 }}>
-            {part2 ? "Output" : "Lowest Seed"}: {output}
+            {part2 ? "Output" : "Product of matches"}: {output}
           </h4>
         </Box>
       )}
