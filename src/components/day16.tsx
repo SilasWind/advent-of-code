22b8f76be3f596d8cd8df002f9afbb9e604783ac
Silas Windhorst <silas.windhorst@tuhh.de>
2023-12-18 12:29:54 +0100

    day 16 part 1

--- src/components/day16.tsx
+++ src/components/day16.tsx
@@ -20,91 +20,95 @@ function DaySixteen() {
     let beams: number[][] = [[0, 0, 0, 0]];
     let counter = 0;
     let newEnergized: number[][] = [];
+    const newEnergizedSet = new Set();
     let lastAddition = 0;
-    while (beams.length) {
-      if (lastAddition + 20 < counter) {
-        console.log("no more additions");
-        break;
-      }
-      beams.forEach((beam, beamIndex) => {
-        if (
-          beam[0] > -1 &&
-          beam[0] < newInputArray[0].length &&
-          beam[1] > -1 &&
-          beam[1] < newInputArray.length
-        ) {
-          if (
-            !newEnergized.some(
-              (checkBeam) =>
-                checkBeam.toString() === [beam[0], beam[1]].toString()
-            )
-          ) {
-            newEnergized.push([beam[0], beam[1]]);
-            lastAddition = counter;
-          }
-          const char = newInputArray[beam[1]][beam[0]];
-          const getDirection = () => {
-            if (beam[3] > beam[1]) {
-              return "up";
-            }
-            if (beam[3] < beam[1]) {
-              return "down";
-            }
-            if (beam[2] > beam[0]) {
-              return "left";
-            } else {
-              return "right";
-            }
-          };
-          const direction = getDirection();
-          const changes = {
-            up: [beam[0], beam[1] - 1, beam[0], beam[1]],
-            down: [beam[0], beam[1] + 1, beam[0], beam[1]],
-            right: [beam[0] + 1, beam[1], beam[0], beam[1]],
-            left: [beam[0] - 1, beam[1], beam[0], beam[1]],
-          };
-          if (char === ".") {
-            beams[beamIndex] = [...changes[direction]];
-          } else if (char === "/") {
-            if (direction === "up") {
-              beams[beamIndex] = [...changes.right];
-            } else if (direction === "down") {
-              beams[beamIndex] = [...changes.left];
-            } else if (direction === "right") {
-              beams[beamIndex] = [...changes.up];
-            } else {
-              beams[beamIndex] = [...changes.down];
-            }
-          } else if (char === "\\") {
-            if (direction === "up") {
-              beams[beamIndex] = [...changes.left];
-            } else if (direction === "down") {
-              beams[beamIndex] = [...changes.right];
-            } else if (direction === "right") {
-              beams[beamIndex] = [...changes.down];
-            } else {
-              beams[beamIndex] = [...changes.up];
-            }
-          } else if (char === "|") {
-            if (direction === "up" || direction === "down") {
-              beams[beamIndex] = changes[direction];
-            } else {
-              beams[beamIndex] = [...changes.down];
-              beams.push([...changes.up]);
-            }
-          } else if (char === "-") {
-            if (direction === "left" || direction === "right") {
-              beams[beamIndex] = [...changes[direction]];
-            } else {
-              beams[beamIndex] = [...changes.right];
-              beams.push([...changes.left]);
-            }
-          }
-        } else {
-          beams.splice(beamIndex, 1);
+    const calculateBeams = () => {
+      while (beams.length) {
+        if (lastAddition + 20 < counter) {
+          console.log("no more additions");
+          break;
         }
-      });
-      counter++;
+        beams.forEach((beam, beamIndex) => {
+          if (
+            beam[0] > -1 &&
+            beam[0] < newInputArray[0].length &&
+            beam[1] > -1 &&
+            beam[1] < newInputArray.length
+          ) {
+            if (!newEnergizedSet.has(`${beam[0]},${beam[1]}`)) {
+              newEnergizedSet.add(`${beam[0]},${beam[1]}`);
+              newEnergized.push([beam[0], beam[1]]);
+              lastAddition = counter;
+            }
+            const char = newInputArray[beam[1]][beam[0]];
+            const getDirection = () => {
+              if (beam[3] > beam[1]) {
+                return "up";
+              }
+              if (beam[3] < beam[1]) {
+                return "down";
+              }
+              if (beam[2] > beam[0]) {
+                return "left";
+              } else {
+                return "right";
+              }
+            };
+            const direction = getDirection();
+            const changes = {
+              up: [beam[0], beam[1] - 1, beam[0], beam[1]],
+              down: [beam[0], beam[1] + 1, beam[0], beam[1]],
+              right: [beam[0] + 1, beam[1], beam[0], beam[1]],
+              left: [beam[0] - 1, beam[1], beam[0], beam[1]],
+            };
+            if (char === ".") {
+              beams[beamIndex] = [...changes[direction]];
+            } else if (char === "/") {
+              if (direction === "up") {
+                beams[beamIndex] = [...changes.right];
+              } else if (direction === "down") {
+                beams[beamIndex] = [...changes.left];
+              } else if (direction === "right") {
+                beams[beamIndex] = [...changes.up];
+              } else {
+                beams[beamIndex] = [...changes.down];
+              }
+            } else if (char === "\\") {
+              if (direction === "up") {
+                beams[beamIndex] = [...changes.left];
+              } else if (direction === "down") {
+                beams[beamIndex] = [...changes.right];
+              } else if (direction === "right") {
+                beams[beamIndex] = [...changes.down];
+              } else {
+                beams[beamIndex] = [...changes.up];
+              }
+            } else if (char === "|") {
+              if (direction === "up" || direction === "down") {
+                beams[beamIndex] = changes[direction];
+              } else {
+                beams[beamIndex] = [...changes.down];
+                beams.push([...changes.up]);
+              }
+            } else if (char === "-") {
+              if (direction === "left" || direction === "right") {
+                beams[beamIndex] = [...changes[direction]];
+              } else {
+                beams[beamIndex] = [...changes.right];
+                beams.push([...changes.left]);
+              }
+            }
+          } else {
+            beams.splice(beamIndex, 1);
+          }
+        });
+        counter++;
+      }
+    };
+    if (part2) {
+      console.log("emini");
+    } else {
+      calculateBeams();
     }
     setEnergized([...newEnergized]);
     setOutput(newEnergized.length);
@@ -128,7 +132,7 @@ function DaySixteen() {
         handleChange={handleChange}
         dayNumber={16}
       />
-      {/* <div style={{ display: "flex", flexDirection: "column" }}>
+      <div style={{ display: "flex", flexDirection: "column" }}>
         {inputArray.map((line, lineIndex) => (
           <div
             style={{ display: "flex", flexDirection: "row" }}
@@ -152,7 +156,7 @@ function DaySixteen() {
             ))}
           </div>
         ))}
-      </div> */}
+      </div>
     </div>
   );
 }
