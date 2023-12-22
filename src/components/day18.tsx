aa42a7b36d2e3c6693b95cc0d948e9641ed436ef
Silas Windhorst <silas.windhorst@tuhh.de>
2023-12-22 11:50:54 +0100

    day 18 part 1 progress

--- src/components/day18.tsx
+++ src/components/day18.tsx
@@ -6,7 +6,7 @@ function DayEighteen() {
   const [part2, setPart2] = useState(false);
   const [inputString, setInputString] = useState("");
   const [output, setOutput] = useState(0);
-  const [holeOutput, setHoleOutput] = useState<boolean[][]>([[true]]);
+  const [holeOutput, setHoleOutput] = useState<string[][]>([["#"]]);
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -19,25 +19,25 @@ function DayEighteen() {
       .trim()
       .split("\n")
       .map((row) => row.trim().split(" "));
-    let diggyHole: boolean[][] = [[true]];
+    let diggyHole: string[][] = [["#"]];
     let lastCoords: number[] = [0, 0];
     let newOutput = 0;
     inputArray.forEach((line) => {
       for (let i = 0; i < parseInt(line[1]); i++) {
         if (line[0] === "R") {
           if (lastCoords[0] === diggyHole[lastCoords[1]].length - 1) {
-            diggyHole[lastCoords[1]].push(true);
+            diggyHole[lastCoords[1]].push("#");
           } else {
-            diggyHole[lastCoords[1]][lastCoords[0] + 1] = true;
+            diggyHole[lastCoords[1]][lastCoords[0] + 1] = "#";
           }
           lastCoords = [lastCoords[0] + 1, lastCoords[1]];
         } else if (line[0] === "L") {
           if (lastCoords[0] === 0) {
             diggyHole.forEach((line, lineIndex) => {
-              line.splice(0, 0, lineIndex === lastCoords[1] ? true : false);
+              line.splice(0, 0, lineIndex === lastCoords[1] ? "#" : ".");
             });
           } else {
-            diggyHole[lastCoords[1]][lastCoords[0] - 1] = true;
+            diggyHole[lastCoords[1]][lastCoords[0] - 1] = "#";
             lastCoords = [lastCoords[0] - 1, lastCoords[1]];
           }
         } else if (line[0] === "U") {
@@ -45,59 +45,61 @@ function DayEighteen() {
             diggyHole.splice(
               0,
               0,
-              Array(diggyHole[diggyHole.length - 1].length).fill(false)
+              Array(diggyHole[diggyHole.length - 1].length).fill(".")
             );
-            diggyHole[0][lastCoords[0]] = true;
+            diggyHole[0][lastCoords[0]] = "#";
           } else {
-            diggyHole[lastCoords[1] - 1][lastCoords[0]] = true;
+            diggyHole[lastCoords[1] - 1][lastCoords[0]] = "#";
             lastCoords = [lastCoords[0], lastCoords[1] - 1];
           }
         } else {
           if (lastCoords[1] === diggyHole.length - 1) {
-            diggyHole.push(Array(diggyHole[lastCoords[1]].length).fill(false));
-            diggyHole[lastCoords[1] + 1][lastCoords[0]] = true;
+            diggyHole.push(Array(diggyHole[lastCoords[1]].length).fill("."));
+            diggyHole[lastCoords[1] + 1][lastCoords[0]] = "#";
           } else {
-            diggyHole[lastCoords[1] + 1][lastCoords[0]] = true;
+            diggyHole[lastCoords[1] + 1][lastCoords[0]] = "#";
           }
           lastCoords = [lastCoords[0], lastCoords[1] + 1];
         }
       }
     });
-    let displayHole = _.cloneDeep(diggyHole);
-    displayHole.forEach((line) => {
-      line.fill(false);
-    });
     diggyHole.forEach((line, lineIndex) => {
       let inside = false;
-      let lastBool = false;
+      let lastChar = ".";
       for (let i = 0; i < line.length; i++) {
         if (!(i in line)) {
-          line[i] = false;
+          line[i] = ".";
         }
       }
       line.forEach((char, charIndex) => {
-        if (char) {
+        // console.log(lastChar);
+        if (char === "#") {
           newOutput++;
-          if (!lastBool) {
+          if (lastChar === ".") {
             inside = !inside;
           }
+          lastChar = char;
         } else {
-          if (lineIndex > 0 && lastBool) {
-            inside = displayHole[lineIndex - 1][charIndex];
+          if (
+            lineIndex > 0 &&
+            lastChar === "#" &&
+            line[charIndex - 2] === "#"
+          ) {
+            inside = diggyHole[lineIndex - 1][charIndex] !== "." ? true : false;
           }
-          lastBool = char;
+          lastChar = char;
           if (inside) {
             newOutput++;
-            displayHole[lineIndex][charIndex] = true;
+            diggyHole[lineIndex][charIndex] = "+";
           }
         }
       });
     });
 
-    setHoleOutput([...displayHole]);
+    setHoleOutput([...diggyHole]);
     setOutput(newOutput);
     console.log(diggyHole);
-    console.log("too low: 42288");
+    console.log("too high: 46339");
   };
   return (
     <div>
@@ -122,7 +124,7 @@ function DayEighteen() {
             }}
           >
             {row.map((char, charIndex) => (
-              <div key={charIndex}>{char ? "#" : "."}</div>
+              <div key={charIndex}>{char}</div>
             ))}
           </div>
         ))}
